a5771618c3c182aab6ae7e4a76957026c21cf9b0
Radim Kocman <radim.kocman@gmail.com>
2015-01-13 21:17:46 +0100

    Input parser syntax

---
+++ dev/parser.jison
@@ -0,0 +1,71 @@
+/*!
+ * Input Parser for LL(k) Parsing Table Generator
+ * https://github.com/Gals42/LLk-Parsing-Table-Generator
+ * Author: Radim Kocman
+ */
+
+/* Lexemes */
+%lex
+%%
+
+\s+                         /* skip whitespace */
+"//".*                      /* skip single-line comments*/
+"/*".*"*/"                  /* skip multi-line comments */
+"%token"                    return 'TOKENDEF'
+"%%"                        return 'BEGIN'
+":"                         return ':'
+"|"                         return '|'
+";"                         return ';'
+[a-zA-Z]\S*                return 'ATOKEN'
+"'"[\S]+"'"|"\""[\S]+"\""   return 'VTOKEN'
+<<EOF>>                     return 'EOF'
+
+/lex
+
+%start start
+
+%% /* Input Grammar */
+
+start 
+    : head body EOF
+    ;
+
+token
+    : ATOKEN
+    | VTOKEN
+    ;
+    
+head
+    : /* eps */
+    | TOKENDEF token head2
+    ;
+
+head2
+    : token head2
+    | head
+    ;
+
+body
+    : BEGIN rule body2
+    ;
+
+body2
+    : /* eps */
+    | rule body2
+    ;
+
+rule
+    : token ':' ruleb ';'
+    ;
+
+ruleb
+    : /* eps */
+    | token ruleb2
+    | '|' ruleb
+    ;
+
+ruleb2
+    : /* eps */
+    | token ruleb2
+    | '|' ruleb
+    ;
---
+++ js/generator.js
@@ -0,0 +1,143 @@
+/*!
+ * LL(k) Parsing Table Generator
+ * https://github.com/Gals42/LLk-Parsing-Table-Generator
+ * Author: Radim Kocman
+ */
+
+
+////
+// GRAMMAR OBJECTS
+//////
+
+var GType = {
+  T : 1, // terminal
+  N : 2  // nonterminal
+};
+
+var GElement = function(type, value) {
+  this.type = type;
+  this.value = value;
+};
+
+var GRule = function() {
+  this.left;
+  this.right = [];
+};
+GRule.prototype.setLeft = function(gelement) {
+  this.left = gelement;
+};
+GRule.prototype.addRight = function(gelement) {
+  this.right.push(gelement);
+};
+
+var Grammar = function() {
+  this.N = [];
+  this.R = [];
+};
+Grammar.prototype.addN = function(gelement) {
+  this.N.push(gelement);
+};
+Grammar.prototype.addR = function(grule) {
+  this.R.push(grule);
+};
+
+
+////
+// INPUT PARSER MODIFICATION
+//////
+
+parser.yy.parseError = function parseError(str, hash) {
+    throw hash.line+1;
+};
+
+
+////
+// PARSING TABLE GENERATOR GUI
+//////
+
+var StatusClass = {
+  INFO : "info",
+  OK : "ok",
+  ERROR : "error"
+};
+
+var PTGConfig = {
+  FULL : "full",
+  COMPACT : "compact"
+};
+
+var PTG = {
+
+  inputG : undefined,
+  k : undefined,
+  config : undefined,
+
+  run: function() {
+    this.setInfo("Processing...");
+    
+    this.k = parseInt($("input[name=k]").val());
+    if (isNaN(this.k) || this.k < 1 || this.k > 100) {
+      this.setError("Error: Invalid k");
+      return;
+    }
+    
+    this.config = $("select[name=result]").val();
+    if (this.config !== PTGConfig.FULL && this.config !== PTGConfig.COMPACT) {
+      this.setError("Error: Invalid output selection");
+      return;
+    }
+    
+    this.inputG = $("textarea[name=grammar]").val();
+    if (this.inputG.length === 0) {
+      this.setError("Error: Empty input grammar");
+      return;
+    }
+    try {
+      parser.parse(this.inputG);
+    } catch (err) {
+      this.setError("Error: Invalid input grammar (error on line "+err+")");
+      return;
+    }
+    
+    this.setOk("OK");
+  },
+
+  statusBar: undefined,
+  setInfo: function(msg) {
+    this.statusBar.text(msg);
+    this.statusBar.attr("class", StatusClass.INFO);
+  },
+  setOk: function(msg) {
+    this.statusBar.text(msg);
+    this.statusBar.attr("class", StatusClass.OK);
+  },
+  setError: function(msg) {
+    this.statusBar.text(msg);
+    this.statusBar.attr("class", StatusClass.ERROR);
+  }
+
+};
+$(function() {
+  PTG.statusBar = $("#status span");
+});
+
+
+////
+// CONTENT SELECT HELPER
+//////
+
+function select_all(el) {
+  if (typeof window.getSelection !== "undefined" && 
+      typeof document.createRange !== "undefined") {
+    var range = document.createRange();
+    range.selectNodeContents(el);
+    var sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+  } else if (typeof document.selection !== "undefined" && 
+    typeof document.body.createTextRange !== "undefined") {
+    var textRange = document.body.createTextRange();
+    textRange.moveToElementText(el);
+    textRange.select();
+  }
+}
